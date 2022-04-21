2970285d6d7acdfb9e75331e5cb26835bf58ddcd
Sebastian Kastner <sebastian_kastner@gmx.net>
2022-04-21 19:50:50 +0200

    update index.php for shared apache host

--- server/public/index.php
+++ server/public/index.php
@@ -2,9 +2,9 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once '../vendor/autoload_runtime.php';
+// require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel('dev', false);
-    // return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
