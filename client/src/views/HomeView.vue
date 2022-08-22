f4a6d11b8fdde7887b8361a9b9b9e0f2007584a2
Sebastian Kastner <sebastian_kastner@gmx.net>
2022-08-22 23:54:04 +0200

    revert changes to HomeView

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,43 +1,14 @@
 <template>
   <div class="home">
-    <div class="container">
-      <form @submit.prevent="onSubmit">
-        <div class="form-group">
-          <input type="file" @change="onFileUpload" />
-        </div>
-        <div class="form-group">
-          <button class="btn btn-primary btn-block btn-lg">Upload File</button>
-        </div>
-      </form>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { MediaObjectClient } from "@/clients/MediaObjectClient";
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
-  components: {},
+  components: {
+  }
 })
-export default class HomeView extends Vue {
-
-  file?: File;
-  client = new MediaObjectClient();
-
-  public onFileUpload(event: Event) {
-    /* eslint-disable */
-    const target = event.target as any;
-    if (target && "files" in target) {
-      this.file = target["files"][0];
-    }
-  }
-
-  public async onSubmit(): Promise<void> {
-    if(this.file) {
-      const obj = await this.client.createMediaObject(this.file);
-      console.log(obj);
-    }
-  }
-}
+export default class HomeView extends Vue {}
 </script>
