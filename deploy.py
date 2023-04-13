1f39a7a3b235e292d685336912cac4233bedfedb
Sebastian Kastner <sebastian_kastner@gmx.net>
2023-04-13 22:21:33 +0200

    run php optimization and cache:clear on server

--- deploy.py
+++ deploy.py
@@ -10,7 +10,12 @@ desc = '''Deployment helper script to deploy kookary sources to a webserver.
 Requires sshpass, ssh, scp, make, php composer and node to be installed on the host system.
 Locally builds the client and server sources using the respective make scripts and replaces the code on the server.
 
-The server is configured by setting the "KOOKARY_SERVER_HOME", "KOOKARY_SERVER_USER_NAME" and "KOOKARY_SERVER_ADDR" as env variables'''
+The server is configured by setting the "KOOKARY_SERVER_HOME", "KOOKARY_SERVER_USER_NAME" and "KOOKARY_SERVER_ADDR" as env variables
+
+For the -clean flag, composer and php will be invoked on the server. To set the paths to the executables, set the env variables 
+"KOOKARY_PHP_CMD" and "KOOKARY_COMPOSER_CMD" (either absolute or relative from the [server_home]/server directory). If not set,
+the default invocation commands will be used.
+'''
 
 parser = argparse.ArgumentParser(prog='kookary deployment', description=desc)
 parser.add_argument('target', choices=['server', 'client', 'all'], help="Target to replace. 'server' (i.e. php code), 'client' (i.e. html, css, js) code or 'all' for both.")
@@ -31,9 +36,8 @@ class CmdExecutor():
             subprocess.run(cmd, cwd=self.cwd, capture_output=False, check=True, shell=True)
             
     def exec_and_get(self, cmd: str) -> str:
-        if not self.simulate:
-            result = subprocess.run(cmd, cwd=self.cwd, stdout=subprocess.PIPE, check=True, shell=True)
-            return result.stdout.decode("utf-8").strip()
+        result = subprocess.run(cmd, cwd=self.cwd, stdout=subprocess.PIPE, check=True, shell=True)
+        return result.stdout.decode("utf-8").strip()
         
     def ssh_exec(self, cmd: str):
         self.exec("sshpass -p {} ssh {}@{} '{}'".format(self.pw, self.user, self.addr, cmd))
@@ -55,16 +59,18 @@ class CmdExecutor():
         # replace password of sshpass in output
         # prints all groups, except for the password
         print(re.sub(r"(sshpass\s+-p\s+)(.+)\s+((?:\bssh\b)|(?:\bscp\b))(.+)", r"\1 *** \3\4", cmd))
+        
+def get_env_var(var_name: str, default: str = None):
+    if var_name in os.environ:
+        return os.environ[var_name]
+    if default is not None:
+        return default
+    print("Env var " + var_name + " needs to be set! Exiting.")
+    exit(-1)
 
-required_env_vars = [ "KOOKARY_SERVER_HOME", "KOOKARY_SERVER_USER_NAME", "KOOKARY_SERVER_ADDR" ]
-for var in required_env_vars:
-    if var not in os.environ:
-        print(var + " env var needs to be set")
-        exit(-1)
-
-server_home = os.environ['KOOKARY_SERVER_HOME']
-server_user = os.environ['KOOKARY_SERVER_USER_NAME']
-server_addr = os.environ['KOOKARY_SERVER_ADDR']
+server_home = get_env_var('KOOKARY_SERVER_HOME')
+server_user = get_env_var('KOOKARY_SERVER_USER_NAME')
+server_addr = get_env_var('KOOKARY_SERVER_ADDR')
 
 args = parser.parse_args()
 
@@ -101,25 +107,27 @@ if args.target == "all" or args.target == "server":
     print(">>> Write current git revision to .server_version file..")
     cmd.ssh_exec("echo " + revision + " > " + server_home + "/server/.server_version")
     
-    # build and upload client
     cmd.chdir(os.path.join(invocation_dir, "server"))
-    cmd.exec("make build")
-    
-    print(">>> Uploading server files..")
-    
+    # TODO: check if "public/bundles" needs to be included here (might be created when running composer install)
     server_uploads = [ "config", "src", "templates", "public/bundles", "public/index.php", "public/.htaccess", "composer.json" ]
-    if args.clean:
-        server_uploads.append("vendor")
-    
-    print(">>> Removing php cache on server..")
-    cmd.ssh_exec("rm -rf " + server_home + "/server/var/cache")
     
     print(">>> Removing php files on server..")
     for upload in server_uploads:
         cmd.ssh_exec("rm -rf " + server_home + "/server/" + upload)
     
-    print(">>> Upload php files to server..");
+    print(">>> Uploading php files to server..");
     for upload in server_uploads:
         cmd.ssh_cp(upload, server_home + "/server/" + upload)
+        
+    php_cmd = get_env_var("KOOKARY_PHP_CMD", "php")
+    composer_cmd = get_env_var("KOOKARY_COMPOSER_CMD", "composer")
+    
+    if args.clean:
+        print(">>> Optimizing php sources on server..")
+        cmd.ssh_exec("cd " + server_home + "/server/ && " + composer_cmd + " dump-autoload --optimize --no-dev --classmap-authoritative")
+        print(">>> Installing php dependencies...")
+        cmd.ssh_exec("cd " + server_home + "/server/ && " + composer_cmd + " install")
+        print(">>> Cleaning php cache on server")
+        cmd.ssh_exec("cd " + server_home + "/server/ && APP_ENV=prod APP_DEBUG=0 " + php_cmd + " bin/console cache:clear")
 
 print(">>> Done")
