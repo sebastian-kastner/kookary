2914a36db343c8c82cb13f70b3f2549e6e6b48e8
Sebastian Kastner <sebastian_kastner@gmx.net>
2023-05-11 19:04:43 +0200

    attempt to clean up main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,27 +1,29 @@
-import Vue from 'vue'
+import { createApp } from "vue";
 import App from './App.vue'
 import router from './router'
 import { rootStore, userStore, initStores } from './stores/rootStore'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import VModal from 'vue-js-modal'
+import { BootstrapVueNext } from 'bootstrap-vue-next'
+// import VModal from 'vue-js-modal'
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-bootstrap.css';
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVueIcons);
-Vue.use(VModal, { dialog: true });
-Vue.use(ToastPlugin, { position: 'bottom', type: 'error', duration: 10000 });
+// Vue.use(BootstrapVue);
+// Vue.use(BootstrapVueIcons);
+
+// createApp(App); --> this does not work for some reason..
+const app = createApp(App, {});
+app.use(rootStore);
+app.use(router);
+app.use(BootstrapVueNext);
+app.use(ToastPlugin, { position: 'bottom', type: 'error', duration: 10000 });
+// Vue.use(VModal, { dialog: true });
 
 initStores();
 
 userStore.initUserLogin().finally(() => {
-  new Vue({
-    router,
-    store: rootStore,
-    render: h => h(App)
-  }).$mount('#app');
+  app.mount('#app');
 });
 
 
