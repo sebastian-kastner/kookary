a707d7db89b242b2ab7e6a15ffe085c3c0aa20fa
Sebastian Kastner <sebastian_kastner@gmx.net>
2023-05-30 19:17:03 +0200

    configure toast plugin and add ToastMixin

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,8 @@ import App from "./App.vue";
 import router from "./router";
 import { addIcons } from "./icons";
 import { rootStore, userStore, initStores } from "./stores/rootStore";
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 const app = createApp(App);
 
@@ -13,8 +15,7 @@ const vfm = createVfm();
 app.use(rootStore);
 app.use(vfm);
 app.use(router);
-// for some reason this breaks the access to this.$router
-// app.use(ToastPlugin, { position: "bottom", type: "error", duration: 10000 });
+app.use(ToastPlugin, { position: "bottom", type: "error", duration: 10000 });
 
 initStores();
 addIcons();
---
+++ client/src/mixins/ToastMixin.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { Vue, ComponentBase } from "vue-facing-decorator";
+import { useToast } from "vue-toast-notification";
+
+@ComponentBase
+export default class ToastMixin extends Vue {
+  $toast = useToast();
+}
+</script>
